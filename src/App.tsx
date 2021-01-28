ae5a2ea96cd4f358172a74913b8b5b8441effff1
zichenma <zma@liaisionedu.com>
2021-01-27 19:01:34 -0500

    4_9_Button_styling_part_2

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button> Hello</Button>
-        <Button btnType={ ButtonType.Primary } size={ButtonSize.Large}> Hello World</Button>
-        <Button btnType={ButtonType.Link} href="https://www.google.com">Google Link</Button>
+        <Button btnType={ ButtonType.Default }>Default</Button>
+        <Button disabled> Disabled</Button>
+        <Button btnType={ ButtonType.Primary } size={ButtonSize.Large}>Primary Large</Button>
+        <Button btnType={ ButtonType.Default } size={ButtonSize.Small}>Default Small</Button>
+        <Button btnType={ ButtonType.Danger } size={ButtonSize.Large}> Danger Large</Button>
+        <Button btnType={ButtonType.Link} href="https://www.google.com" disabled>Google Link(default)</Button>
+        <Button btnType={ButtonType.Link} href="https://www.google.com" >Google Link(disabled)</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
