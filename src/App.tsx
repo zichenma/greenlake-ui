532f8ecddbb488c0b8994dcf14922b434c2b6e53
zichenma <zma@liaisionedu.com>
2021-02-04 01:29:52 -0500

    4_11_Button_styling_better_part_3

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button btnType={ ButtonType.Default }>Default</Button>
-        <Button disabled> Disabled</Button>
+        <Button onClick={e => {e.preventDefault(); alert()}} btnType={ ButtonType.Default }>Default</Button>
+        <Button disabled className="custom"> Disabled</Button>
         <Button btnType={ ButtonType.Primary } size={ButtonSize.Large}>Primary Large</Button>
         <Button btnType={ ButtonType.Default } size={ButtonSize.Small}>Default Small</Button>
         <Button btnType={ ButtonType.Danger } size={ButtonSize.Small}> Danger Large</Button>
         <Button btnType={ButtonType.Link} href="https://www.google.com" disabled>Google Link(default)</Button>
-        <Button btnType={ButtonType.Link} href="https://www.google.com" >Google Link(disabled)</Button>
+        <Button btnType={ButtonType.Link} href="https://www.google.com" target="_blank">Google Link(disabled)</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
